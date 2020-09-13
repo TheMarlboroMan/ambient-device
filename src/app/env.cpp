ef4e287f67123d73dbe078cc01ec81937b812675
The Marlboro Man <marlborometal@gmail.com>
2020-09-13 14:58:44 +0200

    running, still a dump at exit

--- src/app/env.cpp
+++ src/app/env.cpp
@@ -7,6 +7,11 @@ std::string app::get_data_dir() {
 	return std::string{getenv("HOME")}+"/.ambient-device/data/";
 }
 
+std::string app::get_pic_dir() {
+
+	return std::string{getenv("HOME")}+"/.ambient-device/data/pics/";
+}
+
 std::string app::get_log_dir() {
 
 	return std::string{getenv("HOME")}+"/.ambient-device/logs/";
